Corrected masking

models/modules.py
@@ -283,7 +283,8 @@ class TimeInitTransformExp(nn.Module):
             curr_time = curr_time * (1.01)
         lin_out = self.lin(time_diffs.reshape(n, k, 1)/(curr_time-self.min_time)).reshape(n, k)
         zeros = torch.zeros(n, k).to(time_diffs.device)
-        zeros[mask] = lin_out[mask]
+        rows, cols = mask[:, 0], mask[:, 1]
+        zeros[rows, cols] = lin_out[rows, cols]
         exp_out = torch.exp(-torch.square(zeros))
         output = torch.sum(exp_out, dim = 1)
         return output
@@ -307,7 +308,8 @@ class TimeInitTransformLinear(nn.Module):
         mask = torch.argwhere(time_diffs - check_time != 0)
         lin_out = self.lin(time_diffs.reshape(n, k, 1)/(curr_time-self.min_time)).reshape(n, k)
         zeros = torch.zeros(n, k).to(time_diffs.device)
-        zeros[mask] = lin_out[mask]
+        rows, cols = mask[:, 0], mask[:, 1]
+        zeros[rows, cols] = lin_out[rows, cols]
         output = torch.sum(zeros, dim = 1)
         return output
     